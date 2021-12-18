4658ddc3184f8d05ab42049e66ee83552bf9924e
timbrownls20 <timbrownls26@hotmail.com>
2021-12-18 19:42:01 +1000

    update script for node typescript app

--- node-typescript/src/app.test.ts
+++ node-typescript/src/app.test.ts
@@ -4,8 +4,6 @@ const { app, server } = require("./app");
 
 describe("Smoke test", function () {
   test("responds with correct text", function (done) {
-    console.log("test start");
-
     request(app)
       .get("/")
       .expect(200)
