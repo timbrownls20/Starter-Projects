2c0dc998a008fc787ab0daf4317eae5f2893bd19
timbrownls20 <timbrownls26@hotmail.com>
2021-12-18 14:27:55 +1000

    nodemon as start option for node project

--- node-typescript/src/app.ts
+++ node-typescript/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Node with TypeScript!');
+  res.send('Node with TypeScript!!');
 });
 app.listen(port, (err?: any) => {
   if (err) {
