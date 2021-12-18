26f5fa5c3480ed41ffede4e79de7527aa77d10b1
timbrownls20 <timbrownls26@hotmail.com>
2021-12-18 13:42:34 +1000

    fix tests if component has image or css

--- react/src/App.test.js
+++ react/src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 test('App can render', () => {
   render(<App />);
 
-  const title = screen.getByText(/This is my React app/i);
+  const title = screen.getByText(/React app from scratch/i);
   expect(title).toBeInTheDocument();
 
 });
